a2a87005599cce4de7761278b985406ed523c19f
juviluna <115751316+juviluna@users.noreply.github.com>
2023-08-16 13:15:18 -0500

    Julia's react

---
+++ soapbox/frontend/soapbox-frontend/.vite/deps/react-bootstrap_Offcanvas.js
@@ -0,0 +1,13 @@
+import {
+  Offcanvas_default
+} from "./chunk-3T5JN7UZ.js";
+import "./chunk-34V2E43C.js";
+import "./chunk-MDIHFEFG.js";
+import "./chunk-IZFFW5YA.js";
+import "./chunk-D7KWCMHT.js";
+import "./chunk-WTMQV53C.js";
+import "./chunk-L7APZED3.js";
+export {
+  Offcanvas_default as default
+};
+//# sourceMappingURL=react-bootstrap_Offcanvas.js.map
