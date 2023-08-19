3417417590ef78ff52235e9be583bf1bfed25a23
LucJD <ldickerson@basecampcodingacademy.org>
2023-08-18 20:27:11 -0500

    test stripe functions, not currently functioning without publish key

--- soapbox/backend/frontend/src/index.js
+++ soapbox/backend/frontend/src/index.js
@@ -8,13 +8,23 @@ import reportWebVitals from './reportWebVitals';
 import store from './store';
 import { Provider } from 'react-redux';
 
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+
+const stripePromise = loadStripe('sk_test_51NehWPJEf7VDPp6AtCb0LKiTWOUQw8TCq352t25zybV78LGlVlTyiTsn2pAeNnZR6jKC5bxittSuZ5rbI66Mde9200WtUASyNy')
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Elements stripe = {stripePromise}>
+    <Provider store={store}>
     <App />
 
   </Provider>
+  </Elements>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
--- soapbox/backend/frontend/src/screens/OrderScreen.js
+++ soapbox/backend/frontend/src/screens/OrderScreen.js
@@ -1,10 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Elements} from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import { CardElement } from '@stripe/react-stripe-js'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { getOrderDetails, payOrder } from '../actions/orderActions'
+import Loader from '../components/Loader'
+import Messages from '../components/Messages'
+import { Row, Col, Button, ListGroup, Card, Image } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { deliverOrder } from '../actions/orderActions'
+import { ORDER_PAY_RESET } from '../constants/orderConstants'
+import { ORDER_DELIVER_RESET } from '../constants/orderConstants'
+
+
 
 function OrderScreen() {
+
+  const {id} = useParams()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [sdkReady, setSdkReady] = useState(false)
+
+  const orderDetails = useSelector(state => state.orderDetails)
+  const {order, error, loading } = orderDetails
+
+  const orderPay = useSelector(state => state.orderPay)
+  const {loading: loadingPay, success: successPay} = orderPay
+
+
+  const orderDeliver = useSelector(state => state.orderDeliver)
+  const {loading: loadingDeliver, success: successDeliver} = orderDeliver
+
+  const userLogin = useSelector(state => state.userLogin)
+  const {userInfo} = userLogin
+
+
+  if (!loading && !error) {
+    order.itemsPrice = order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
+}
+
+  const addPayPalScript = () => {
+    const script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = 'https://www.paypal.com/sdk/js?client-id=AeDXja18CkwFUkL-HQPySbzZsiTrN52cG13mf9Yz7KiV2vNnGfTDP0wDEN9sGlhZHrbb_USawcJzVDgn'
+        script.async = true
+        script.onload = () => {
+            setSdkReady(true)
+        }
+        document.body.appendChild(script)
+  }
+
+  useEffect(() => {
+
+    if (!userInfo) {
+        navigate('/login')
+    }
+
+    if (!order || successPay || order._id !== Number(id) || successDeliver) {
+        dispatch({ type: ORDER_PAY_RESET })
+        dispatch({ type: ORDER_DELIVER_RESET })
+
+        dispatch(getOrderDetails(id))
+    } else if (!order.isPaid) {
+        if (!window.paypal) {
+            addPayPalScript()
+        } else {
+            setSdkReady(true)
+        }
+    }
+}, [dispatch, order, id, successPay, successDeliver])
+
+const successPaymentHandler = (paymentResult) => {
+  dispatch(payOrder(id, paymentResult))
+}
+
+const deliverHandler = () => {
+  dispatch(deliverOrder(order))
+}
+
   return (
-    <div>
-      Order Screen
-    </div>
+   <CardElement/>
+      
   )
 }
 
