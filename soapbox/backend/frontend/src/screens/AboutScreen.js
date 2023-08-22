05fb297ffecc5fbb33166cec9efa81dd0ea06340
juviluna <115751316+juviluna@users.noreply.github.com>
2023-08-21 19:20:52 -0500

    about page info

--- soapbox/backend/frontend/src/screens/AboutScreen.js
+++ soapbox/backend/frontend/src/screens/AboutScreen.js
@@ -1,45 +1,71 @@
-import React from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { Link } from "react-router-dom";
+import Col from "react-bootstrap/Col";
 
-function AboutScreen() {
+function About() {
   return (
-    <div className="login template d-flex justify-content-center align-items-center vh-100 bg-primary background-image">
-      <div className="form_container p-5 rounded bg-white">
-        <Form>
-          <h1 className="text-center signIn-color">Sign In</h1>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-
-            <Col xs="auto" className="d-grid">
-              <Button type="submit" className="btn btn-primary">
-                Submit
-              </Button>
-            </Col>
-            <Row className="py-3">
-              <Col>
-                New Customer?{" "}
-                <Link
-                  className=" text-right register-color"
-                  to={"#"} // to={redirect ? `/register?redirect=${redirect}` : "/register"}
-                >
-                  Register
-                </Link>
-              </Col>
-            </Row>
-          </Form.Group>
-        </Form>
-      </div>
-    </div>
+    <Container>
+      <h1 className="title">About us</h1>
+      <Row>
+        <Col xs={12} lg={6}>
+          <img
+            className="about-pic"
+            src={require("../js-images/sarah.jpg")}
+            alt="Sarah"
+          />
+          <h2>Sarah</h2>
+          <p>
+            {`Welcome to my about me page! I'm thrilled to share a glimpse into my
+            life's journey and passion for education and crafting handmade soap.
+            As a wife and mom to eight wonderful children, my life is a joyful
+            whirlwind of love and laughter. With over 20 years of experience in
+            the education field, I hold a master's degree in teaching, which has
+            allowed me to impact countless young minds positively. Witnessing
+            the growth and development of my students has been immensely
+            rewarding, and I cherish the opportunity to shape their futures.
+            However, my passion for creating handmade soap stems from a deeply
+            personal experience. Growing up, my siblings struggled with
+            sensitive skin, and when I became a mother, I discovered that my
+            children shared the same delicate skin and allergies. Determined to
+            find a solution, I delved into the world of soap-making. Crafting
+            each soap bar by hand not only became a creative outlet but also
+            empowered me to take control of what my family puts on their skin.
+            By carefully selecting natural ingredients, I ensure our handmade
+            soaps are free from harsh chemicals and irritants. This knowledge
+            brings me immense satisfaction and peace of mind, knowing I can
+            provide a safe and nurturing experience for my loved ones. Through
+            my journey as an educator and soap maker, I've come to appreciate
+            the importance of understanding the ingredients and processes
+            involved in our products. I hope to extend this knowledge to others,
+            guiding them toward a healthier and more conscious lifestyle. Thank
+            you for visiting my page, and I invite you to explore further to
+            discover the wonders of handmade soap and the boundless joys of
+            education.`}
+          </p>
+        </Col>
+        <Col xs={12} lg={6}>
+          <img
+            className="about-pic"
+            src={require("../js-images/jojo.jpg")}
+            alt="Jo jo"
+          />
+          <h2>Jo Jo</h2>
+          <p>
+            {`With a rich background spanning 17 years in the retail industry, I
+            bring a wealth of experience and a keen understanding of customer
+            needs to my newest venture. Motivated by personal experiences, my
+            decision to delve into creating handmade soap stems from the
+            challenges my family's sensitive skin has posed. This journey
+            reflects my dedication to addressing a real-world concern and
+            offering a solution that prioritizes quality and care. My transition
+            from retail to crafting skincare products showcases my adaptability
+            and commitment to providing products that truly resonate with my
+            customers.`}
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default AboutScreen;
+export default About;
