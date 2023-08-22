7691c488c50ed637ee2d72b681a6418b5a1897fb
Luis Ortiz <luis.ortiz0320@gmail.com>
2023-08-21 20:03:19 -0500

    fixed a couple of things

--- soapbox/backend/base/urls/product_urls.py
+++ soapbox/backend/base/urls/product_urls.py
@@ -9,9 +9,10 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', views.getProducts, name='products'),
     path('category/<str:category>/', views.getProductsByCategory, name='product-category'),
+    path('create/', views.createProduct, name='product-create'),
     path('<str:pk>/', views.getProduct, name='product'),
     
-    path('create/', views.createProduct, name='product-create'),
+
     path('upload/', views.uploadImage, name='image-upload'),
     path('update/<str:pk>/', views.updateProduct, name='product-update'),
     path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
