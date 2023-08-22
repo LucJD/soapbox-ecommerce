48c6e87b926df107f5d8ae5139bda71e9602876c
LucJD <ldickerson@basecampcodingacademy.org>
2023-08-21 22:57:16 -0500

    changed search parameters

--- soapbox/backend/frontend/src/components/Header.js
+++ soapbox/backend/frontend/src/components/Header.js
@@ -59,25 +59,25 @@ function Header() {
                   title="Shop"
                   id={`offcanvasNavbarDropdown-expand-${"md"}`}
                 >
-                  <NavDropdown.Item href="/?category=soap">
+                  <NavDropdown.Item href="shop/?category=soap">
                     Bar Soaps
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#bathbomb">
+                  <NavDropdown.Item href="shop?category=bathbombs">
                     Bath Bombs
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#beardbalm">
+                  <NavDropdown.Item href="shop?category=beardbalm">
                     Beard Balms
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="/?category=beardoils">
+                  <NavDropdown.Item href="shop/?category=beardoils">
                     Beard Oils
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#bodybutter">
+                  <NavDropdown.Item href="shop/?category=bodybutter">
                     Body Butters
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="/?category=sugarscrubs">
+                  <NavDropdown.Item href="shop/?category=sugarscrubs">
                     Sugar Scrubs
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#=waxmelts">
+                  <NavDropdown.Item href="shop/?category=waxmelts">
                     Wax Melts
                   </NavDropdown.Item>
                   <NavDropdown.Item href="/custom-orders">
